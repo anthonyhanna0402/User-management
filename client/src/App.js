a2007fad886d2da0c9e416a6afe20e14a0f7b146
Anthony <anthonyhanna0402@gmail.com>
2024-11-24 12:29:19 -0500

    initial

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react'
+import './App.css';
+
+const App = () => {
+  return (
+    <>
+    <h1></h1>
+    </>
+  );
+}
+
+export default App;
