1007ca1afb49f9573467d7d188017e96d1c00b19
Anthony <anthonyhanna0402@gmail.com>
2024-11-24 14:11:29 -0500

    login function complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import {Provider} from 'react-redux';
 import {BrowserRouter as Router, Route, Routes} from 'react'
 import './App.css';
+import store from './app/Store';
+import Home from './Pages/Home';
+import Login from './Pages/Login';
+import Signup from './Pages/Signup';
+import Profile from './Pages/Profile'
 
 const App = () => {
   return (
     <>
-    <h1></h1>
+      <Provider store={store}>
+        <Router>
+          <Routes>
+            <Route path="/register" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/profile" element={<Profile />}>
+          </Routes>
+        </Router>
+      </Provider>      
     </>
   );
 }
